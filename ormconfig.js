141ce743d962e5c7e2b645a175e5a2f459bd80af
r3nanp <renanmol87@gmail.com>
2021-03-17 15:32:34 -0300

    chor(ormconfig): create config

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  type: 'postgres',
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
+  cli: {
+    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+  },
+}
