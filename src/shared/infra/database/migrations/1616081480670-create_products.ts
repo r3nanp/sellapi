327eff7bb7c44fed81207f660d1849928d3e10f8
r3nanp <renanmol87@gmail.com>
2021-03-18 18:44:28 -0300

    feat(create product): add product migration

---
+++ src/shared/infra/database/migrations/1616081480670-create_products.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createProducts1616081480670 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2
+          },
+          {
+            name: 'quantity',
+            type: 'int'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('create_products')
+  }
+}
