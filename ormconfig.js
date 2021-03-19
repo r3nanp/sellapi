7f717acb773a37a53dbcdbccbe3a76be343d715d
r3nanp <renanmol87@gmail.com>
2021-03-19 13:36:30 -0300

    refactor: add host

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,6 @@
 module.exports = {
   type: 'postgres',
+  host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
