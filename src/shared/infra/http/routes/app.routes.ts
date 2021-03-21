5c1d55d58ca7954aa466cbec600c13a5c7503675
r3nanp <renanmol87@gmail.com>
2021-03-21 12:57:46 -0300

    feat(routes): add products router

--- src/shared/infra/http/routes/app.routes.ts
+++ src/shared/infra/http/routes/app.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import { productsRouter } from '@modules/products/infra/http/routes/product.routes'
 
 const routes = Router()
+
+routes.use('/products', productsRouter)
 
 export { routes }
