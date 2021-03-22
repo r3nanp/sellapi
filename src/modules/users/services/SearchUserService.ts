342ebceebe02c4d859cb09ac29fb186f2dc4a247
r3nanp <renanmol87@gmail.com>
2021-03-22 16:37:06 -0300

    feat(services): create search service

---
+++ src/modules/users/services/SearchUserService.ts
@@ -0,0 +1,19 @@
+import { getCustomRepository } from 'typeorm'
+
+import { Service } from '@shared/core/Service'
+import { User } from '../infra/typeorm/entities/user.entity'
+import { UserRepository } from '../infra/typeorm/repositories/UserRepository'
+
+type Response = {
+  users: User[]
+}
+
+export class SearchUserService implements Service<void, Response> {
+  async execute(): Promise<Response> {
+    const usersRepository = getCustomRepository(UserRepository)
+
+    const users = await usersRepository.find()
+
+    return { users }
+  }
+}
