0dc208957b22c03c4c2acd05cf7900c7afaa1672
r3nanp <renanmol87@gmail.com>
2021-03-22 16:38:55 -0300

    feat(app routes): create app routes

--- src/shared/infra/http/routes/app.routes.ts
+++ src/shared/infra/http/routes/app.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
+
 import { productsRouter } from '@modules/products/infra/http/routes/product.routes'
+import { usersRouter } from '@modules/users/infra/http/routes/users.routes'
 
 const routes = Router()
 
 routes.use('/products', productsRouter)
+routes.use('/users', usersRouter)
 
 export { routes }
