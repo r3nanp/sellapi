cc1deb358b68872833726e67a2f5c1952a9a3f32
r3nanp <renanmol87@gmail.com>
2021-03-22 14:26:21 -0300

    refactor: add to typeorm folder

---
+++ src/shared/infra/typeorm/migrations/1616428026870-create_users.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createUsers1616428026870 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
