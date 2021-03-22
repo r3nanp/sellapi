5915c724ed9874b4fc3d4b470a83ac717721246b
r3nanp <renanmol87@gmail.com>
2021-03-22 16:35:58 -0300

    feat(users): create user repository

---
+++ src/modules/users/infra/typeorm/repositories/UserRepository.ts
@@ -0,0 +1,37 @@
+import { EntityRepository, Repository } from 'typeorm'
+import { User } from '../entities/user.entity'
+
+type Response = User | undefined
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async findByName(name: string): Promise<Response> {
+    const user = await this.findOne({
+      where: {
+        name
+      }
+    })
+
+    return user
+  }
+
+  async findByEmail(email: string): Promise<Response> {
+    const user = await this.findOne({
+      where: {
+        email
+      }
+    })
+
+    return user
+  }
+
+  async findById(id: string): Promise<Response> {
+    const user = await this.findOne({
+      where: {
+        id
+      }
+    })
+
+    return user
+  }
+}
