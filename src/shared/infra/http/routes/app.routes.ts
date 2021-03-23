33299812038d7716d3c14f65166884f125808fb2
r3nanp <renanmol87@gmail.com>
2021-03-23 15:07:43 -0300

    feat(app): create session router

--- src/shared/infra/http/routes/app.routes.ts
+++ src/shared/infra/http/routes/app.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import { productsRouter } from '@modules/products/infra/http/routes/product.routes'
 import { usersRouter } from '@modules/users/infra/http/routes/users.routes'
+import { sessionsRouter } from '@modules/users/infra/http/routes/session.routes'
 
 const routes = Router()
 
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
 
 export { routes }
