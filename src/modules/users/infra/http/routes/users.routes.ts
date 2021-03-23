05c7608c4237bec06acb809986475e9b24291f7d
r3nanp <renanmol87@gmail.com>
2021-03-23 15:06:30 -0300

    feat(users): add ensure middleware

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -2,10 +2,13 @@ import { Router } from 'express'
 
 import { createUserController } from '@modules/users/useCases/CreateUser'
 import { searchUserController } from '@modules/users/useCases/SearchUser'
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
 
-usersRouter.get('/', searchUserController.index)
 usersRouter.post('/', createUserController.create)
+
+usersRouter.use(ensureAuthenticated)
+usersRouter.get('/', searchUserController.index)
 
 export { usersRouter }
