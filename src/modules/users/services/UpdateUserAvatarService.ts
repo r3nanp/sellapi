a07f90abc4535bdd75d50d7fb0156943e252b761
r3nanp <renanmol87@gmail.com>
2021-03-23 17:39:47 -0300

    feat(services): create user upload service

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,43 @@
+import { getCustomRepository } from 'typeorm'
+import path from 'path'
+import fsPromises from 'fs/promises'
+import uploadConfig from '@config/upload'
+
+import { Service } from '@shared/core/Service'
+import { AppError } from '@shared/errors/AppError'
+import { UserRepository } from '../infra/typeorm/repositories/UserRepository'
+import { User } from '../infra/typeorm/entities/user.entity'
+
+interface Request {
+  user_id: string
+  avatarFilename: string
+}
+
+type Response = User
+
+export class UpdateUserAvatarService implements Service<Request, Response> {
+  async execute({ avatarFilename, user_id }: Request): Promise<Response> {
+    const usersRepository = getCustomRepository(UserRepository)
+
+    const user = await usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new AppError('User not found.')
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
+      const userAvatarFileExists = await fsPromises.stat(userAvatarFilePath)
+
+      if (userAvatarFileExists) {
+        await fsPromises.unlink(userAvatarFilePath)
+      }
+    }
+
+    user.avatar = avatarFilename
+
+    await usersRepository.save(user)
+
+    return user
+  }
+}
