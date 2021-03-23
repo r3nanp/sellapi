b2a067fc7dd8d0911ebc78d4936df85aa36a0795
r3nanp <renanmol87@gmail.com>
2021-03-23 17:48:38 -0300

    feat(users routes): add avatar route and middleware

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,14 +1,24 @@
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from '@config/upload'
 
 import { createUserController } from '@modules/users/useCases/CreateUser'
 import { searchUserController } from '@modules/users/useCases/SearchUser'
+import { userAvatarController } from '@modules/users/useCases/UserAvatar'
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
+
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', createUserController.create)
 
 usersRouter.use(ensureAuthenticated)
 usersRouter.get('/', searchUserController.index)
+usersRouter.patch(
+  '/avatar',
+  upload.single('avatar'),
+  userAvatarController.update
+)
 
 export { usersRouter }
