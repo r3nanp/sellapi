ec190a38a84a1d5bfc8ebdc682d5651cd9164b90
r3nanp <renanmol87@gmail.com>
2021-03-24 12:07:31 -0300

    feat: create user_tokens migration

---
+++ src/shared/infra/typeorm/migrations/1616589315135-user_tokens.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class userTokens1616589315135 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'token',
+            type: 'uuid'
+          },
+          {
+            name: 'user_id',
+            type: 'uuid'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'FKTokenUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_tokens')
+  }
+}
