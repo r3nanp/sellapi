8932e43816354722de55a17242348858f996db6b
r3nanp <renanmol87@gmail.com>
2021-03-24 12:09:33 -0300

    feat(repositories): create user tokens repository

---
+++ src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,27 @@
+import { EntityRepository, Repository } from 'typeorm'
+import { UserTokens } from '../entities/UserTokens.entity'
+
+type Response = UserTokens | undefined
+
+@EntityRepository(UserTokens)
+export class UserTokensRepository extends Repository<UserTokens> {
+  async findByToken(token: string): Promise<Response> {
+    const userToken = await this.findOne({
+      where: {
+        token
+      }
+    })
+
+    return userToken
+  }
+
+  async generate(user_id: string): Promise<Response> {
+    const userToken = this.create({
+      user_id
+    })
+
+    await this.save(userToken)
+
+    return userToken
+  }
+}
