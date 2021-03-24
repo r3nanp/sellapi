c2fccca16c9fb10091ef8943e7709ebef9bc53d8
r3nanp <renanmol87@gmail.com>
2021-03-24 12:13:40 -0300

    chore: add password routes

--- src/shared/infra/http/routes/app.routes.ts
+++ src/shared/infra/http/routes/app.routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import { productsRouter } from '@modules/products/infra/http/routes/product.routes'
 import { usersRouter } from '@modules/users/infra/http/routes/users.routes'
 import { sessionsRouter } from '@modules/users/infra/http/routes/session.routes'
+import { passwordRouter } from '@modules/users/infra/http/routes/password.routes'
 
 const routes = Router()
 
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
+routes.use('/password', passwordRouter)
 
 export { routes }
