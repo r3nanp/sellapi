fb13af5ab982d12cd5ffa90ddb08c699e3a4b09c
r3nanp <renanmol87@gmail.com>
2021-03-25 18:28:42 -0300

    feat(config): add readfile function to read the mail template

--- src/config/mailTemplate.ts
+++ src/config/mailTemplate.ts
@@ -1,17 +1,22 @@
 import handlebars from 'handlebars'
+import fs from 'fs/promises'
 
 interface TemplateVariable {
   [key: string]: string | number
 }
 
 export interface IParseMailTemplate {
-  template: string
+  file: string
   variables: TemplateVariable
 }
 
 export class MailTemplate {
-  async parse({ template, variables }: IParseMailTemplate): Promise<string> {
-    const parseTemplate = handlebars.compile(template)
+  async parse({ file, variables }: IParseMailTemplate): Promise<string> {
+    const templateFileContent = await fs.readFile(file, {
+      encoding: 'utf-8'
+    })
+
+    const parseTemplate = handlebars.compile(templateFileContent)
 
     return parseTemplate(variables)
   }
