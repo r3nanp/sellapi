301bb1308f8ca39e1c742fcd332a00545ceffb5c
r3nanp <renanmol87@gmail.com>
2021-03-26 09:08:46 -0300

    feat(app routes): add profile routes

--- src/shared/infra/http/routes/app.routes.ts
+++ src/shared/infra/http/routes/app.routes.ts
@@ -4,6 +4,7 @@ import { productsRouter } from '@modules/products/infra/http/routes/product.rout
 import { usersRouter } from '@modules/users/infra/http/routes/users.routes'
 import { sessionsRouter } from '@modules/users/infra/http/routes/session.routes'
 import { passwordRouter } from '@modules/users/infra/http/routes/password.routes'
+import { profileRouter } from '@modules/users/infra/http/routes/profile.routes'
 
 const routes = Router()
 
@@ -11,5 +12,6 @@ routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
 routes.use('/password', passwordRouter)
+routes.use('/profile', profileRouter)
 
 export { routes }
