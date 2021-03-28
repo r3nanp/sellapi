921b31f524f7decc45f496de07a8b6a14bba0ad6
r3nanp <renanmol87@gmail.com>
2021-03-28 15:49:00 -0300

    feat(services): create search customer service

---
+++ src/modules/customers/services/SearchCustomerService.ts
@@ -0,0 +1,17 @@
+import { getCustomRepository } from 'typeorm'
+
+import { Service } from '@shared/core/Service'
+import { Customer } from '../infra/typeorm/entities/customer.entity'
+import { CustomersRepository } from '../infra/typeorm/repositories/CustomersRepository'
+
+type Response = Customer[]
+
+export class CreateCustomerService implements Service<Request, Response> {
+  async execute(): Promise<Response> {
+    const customersRepository = getCustomRepository(CustomersRepository)
+
+    const customer = await customersRepository.find()
+
+    return customer
+  }
+}
