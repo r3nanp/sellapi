8be4ffe7871e91e2879195e1bea83ad21940e5bc
r3nanp <renanmol87@gmail.com>
2021-03-28 16:56:18 -0300

    feat(routes): create customer routes

---
+++ src/modules/customers/infra/http/routes/customers.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+
+import { searchCustomerController } from '@modules/customers/useCases/SearchCustomer'
+import { showCustomerController } from '@modules/customers/useCases/ShowCustomer'
+import { createCustomerController } from '@modules/customers/useCases/CreateCustomer'
+import { updateCustomerController } from '@modules/customers/useCases/UpdateCustomer'
+import { deleteCustomerController } from '@modules/customers/useCases/DeleteCustomer'
+
+const customersRouter = Router()
+
+customersRouter.get('/', searchCustomerController.search)
+customersRouter.get('/:id', showCustomerController.show)
+
+customersRouter.post('/', createCustomerController.create)
+customersRouter.put('/', updateCustomerController.update)
+customersRouter.delete('/', deleteCustomerController.delete)
+
+export { customersRouter }
