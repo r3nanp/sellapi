189b4c6d824d59e128e20c8ced0a240e97708892
r3nanp <renanmol87@gmail.com>
2021-03-30 18:50:39 -0300

    feat(product entity): add relations configuration

--- src/modules/products/infra/typeorm/entities/product.entity.ts
+++ src/modules/products/infra/typeorm/entities/product.entity.ts
@@ -2,10 +2,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryColumn,
   UpdateDateColumn
 } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
+import { OrdersProducts } from '@modules/orders/infra/typeorm/entities/orders_products.entity'
 
 @Entity('products')
 export class Product {
@@ -17,6 +19,9 @@ export class Product {
 
   @PrimaryColumn()
   readonly id: string
+
+  @OneToMany(() => OrdersProducts, orders_products => orders_products.product)
+  orders_products: OrdersProducts[]
 
   @Column()
   name: string
