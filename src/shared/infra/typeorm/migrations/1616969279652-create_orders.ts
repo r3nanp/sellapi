078d8d2140d8fd0dbc86203ad6845a336d96cc5d
r3nanp <renanmol87@gmail.com>
2021-03-30 18:10:33 -0300

    feat(migrations): create orders migration

---
+++ src/shared/infra/typeorm/migrations/1616969279652-create_orders.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createOrders1616969279652 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders')
+  }
+}
