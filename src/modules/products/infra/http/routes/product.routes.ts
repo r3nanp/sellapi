de54d4c11aaab06db1bb280da9eb3f80301a81f2
r3nanp <renanmol87@gmail.com>
2021-03-30 18:52:30 -0300

    feat(products routes): add middleware

--- src/modules/products/infra/http/routes/product.routes.ts
+++ src/modules/products/infra/http/routes/product.routes.ts
@@ -5,12 +5,14 @@ import { searchProductController } from '@modules/products/useCases/SearchProduc
 import { showProductController } from '@modules/products/useCases/ShowProduct'
 import { updateProductController } from '@modules/products/useCases/UpdateProduct'
 import { deleteProductController } from '@modules/products/useCases/DeleteProduct'
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated'
 
 const productsRouter = Router()
 
 productsRouter.get('/', searchProductController.index)
 productsRouter.get('/:id', showProductController.show)
 
+productsRouter.use(ensureAuthenticated)
 productsRouter.post('/', createProductController.create)
 productsRouter.put('/:id', updateProductController.update)
 productsRouter.delete('/:id', deleteProductController.delete)
