b6cc774f481ae119c60978756289425f813a5341
r3nanp <renanmol87@gmail.com>
2021-03-30 18:53:07 -0300

    feat(app routes): add orders routes

--- src/shared/infra/http/routes/app.routes.ts
+++ src/shared/infra/http/routes/app.routes.ts
@@ -5,6 +5,8 @@ import { usersRouter } from '@modules/users/infra/http/routes/users.routes'
 import { sessionsRouter } from '@modules/users/infra/http/routes/session.routes'
 import { passwordRouter } from '@modules/users/infra/http/routes/password.routes'
 import { profileRouter } from '@modules/users/infra/http/routes/profile.routes'
+import { customersRouter } from '@modules/customers/infra/http/routes/customers.routes'
+import { ordersRouter } from '@modules/orders/infra/http/routes/order.routes'
 
 const routes = Router()
 
@@ -13,5 +15,7 @@ routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
 routes.use('/password', passwordRouter)
 routes.use('/profile', profileRouter)
+routes.use('/customers', customersRouter)
+routes.use('/orders', ordersRouter)
 
 export { routes }
