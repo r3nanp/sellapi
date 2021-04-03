badc40a87ade483615076524e4bf694bb3fb2cfe
r3nanp <renanmol87@gmail.com>
2021-04-03 13:31:08 -0300

    feat: create bcrypt adapter

---
+++ src/shared/infra/cryptography/BcryptAdapter.ts
@@ -0,0 +1,15 @@
+import { Hasher } from '@shared/core/Hasher'
+import { HashComparer } from '@shared/core/HasherComparer'
+import bcrypt from 'bcryptjs'
+
+export class BcryptAdapter implements Hasher, HashComparer {
+  constructor(private readonly salt: number) {}
+
+  async hash(plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt)
+  }
+
+  async compare(plaintext: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plaintext, digest)
+  }
+}
