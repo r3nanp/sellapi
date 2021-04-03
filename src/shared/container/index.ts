7477e6beecfa295f9296335435f1fbba6ff1d379
r3nanp <renanmol87@gmail.com>
2021-04-03 13:30:34 -0300

    feat: add another repositories to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,15 @@
 import { container } from 'tsyringe'
+
+import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository'
+import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository'
 import { ICustomerRepository } from '@modules/customers/domain/repositories/ICustomerRepository'
 import { CustomersRepository } from '@modules/customers/infra/typeorm/repositories/CustomersRepository'
+import { IUserTokens } from '@modules/users/domain/models/UserTokens'
+import { UserTokens } from '@modules/users/infra/typeorm/entities/UserTokens.entity'
+
+container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
+
+container.registerSingleton<IUserTokens>('UserTokens', UserTokens)
 
 container.registerSingleton<ICustomerRepository>(
   'CustomersRepository',
